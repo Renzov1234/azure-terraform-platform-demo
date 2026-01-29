de4997ac21ceae1250b8438b87421692ced2f85c
Renzov1234 <165003613+Renzov1234@users.noreply.github.com>
2026-01-29 19:10:25 +0100

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate-demo"
-    storage_account_name = "sttfstateXYZ123"
+    storage_account_name = "sttfstate260806"
     container_name       = "tfstate"
     key                  = "demo-functions-dev.tfstate"
   }
